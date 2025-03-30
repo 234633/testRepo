78ea58790533cb24814dc626a2fae177abac3fd7
B4mb00 <ostigor2004@gmail.com>
2025-03-30 02:53:10 +0200

    firstBranch

--- src/main/java/org/example/AtmOptions.java
+++ src/main/java/org/example/AtmOptions.java
@@ -5,5 +5,7 @@ public class AtmOptions {
     public static final String WITHDRAW = "2";
     public static final String DEPOSIT = "3";
     public static final String TRANSFER = "4";
-    public static final String EXIT = "5";
+    public static final String SIGNOUT = "5";
+    public static final String EXIT = "6";
+
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -49,83 +49,119 @@ public class Main {
 //        System.err.println(user1.getPassword());
 //        System.err.println("123");
     }
-    public static void operations(List<User> users){
+    public static void operations(List<User> users) {
         Scanner scanner = new Scanner(System.in);
-        double amount;
+        User loggedInUser = null;
         String finalTouch;
-        User logInUser = null;
 
-        while (true) {
-            for (int i = 2; i >= 0; i--) {
-                System.out.println("Enter your login: ");
-                String login = scanner.next();
-                System.out.println("Enter your password: ");
-                String password = scanner.next();
-                for (User user : users){
-                    if (user.getLogin().equals(login) && user.getPassword().equals(password))
-                        logInUser = user;}
+        for (int i = 3; i >= 0; i--) {
+            System.out.print("Enter your login: ");
+            String login = scanner.next();
+            System.out.print("Enter your password: ");
+            String password = scanner.next();
 
-                if (logInUser != null) {
-                    System.out.println("Login successful.\n");
-                    break;}
-                    else System.out.println("Login failed. Remaining attemts: " + i);
+            for (User user : users) {
+                if (user.getLogin().equals(login) && user.getPassword().equals(password)) {
+                    loggedInUser = user;
+                    break;
+                }
             }
+
+            if (loggedInUser != null) {
+                System.out.println("Login successful. Welcome, User " + loggedInUser.getLogin() + " !\n");
+                break;
+            } else if (i > 0) System.out.println("Login failed. Remaining attempts: " + i);
+                else { System.err.println("Too many attempts, your account is closed. Please contact: 8 800 555 35 ");
+                System.exit(0);
+                }
         }
 
-//        if (user.getBalance() < 0) {
-//            System.out.println("XD you are poor\n");
-//            return;
-//        } else System.out.println("Gosh, you are rich with: " + user.getBalance() + "$\n");
-//
-//        while (true) {
-//            System.out.println("Report your intentions: Balance (1), Withdraw (2), Deposit (3), Transfer (4) or Sign out from the account (5)");
-//            String intention = scanner.next();
-//
-//            switch (intention) {
-//                case AtmOptions.BALANCE:
-//                    System.out.println("Balance: " + user.getBalance() + "\n");
-//                    break;
-//                case WITHDRAW:
-//                case DEPOSIT:
-//                    updateBalance(scanner, intention, user);
-//                    break;
-//                case TRANSFER:
-//                    System.out.println("How much to transfer?: ");
-//                    amount = scanner.nextDouble();
-//                    user.setBalance(user.getBalance() - amount);
-//
-//                    user.setBalance(user.getBalance() + amount);
-//                    System.out.println("Transfered " + amount + " to another account\nCurrent balance: " + user.getBalance() + "\nTransfered successfuly\n");
-//                    break;
-//                case EXIT:
-//                    System.err.println("Do you want to sign out from your account? Y / N");
-//                    finalTouch = scanner.next();
-//                    if (finalTouch.equals("Y")) {
-//                        System.exit(0);
-//                        System.out.println("Thank you for using our servises!\n Goodbye :D ");
-//                    }
-//                    else break;
-//                default:
-//                    System.out.println("Invalid option");
-//                    break;
-//            }
-//        }
+
+        if (loggedInUser.getBalance() < 0) {
+            System.out.println("XD you are poor\n");
+            return;
+        } else System.out.println("Gosh, you are rich with: " + loggedInUser.getBalance() + "$\n");
+
+        while (true) {
+            System.out.println("Report your intentions: Balance (1), Withdraw (2), Deposit (3), Transfer (4), Sign out from the account (5) or Exit (6)");
+            String intention = scanner.next();
+
+            switch (intention) {
+                case AtmOptions.BALANCE:
+                    System.out.println("Balance: " + loggedInUser.getBalance() + "\n");
+                    break;
+                case WITHDRAW:
+                case DEPOSIT:
+                    updateBalance(scanner, intention, loggedInUser);
+                    break;
+                case TRANSFER:
+                    transfers(loggedInUser, scanner, users);
+                    break;
+                case SIGNOUT:
+                    System.err.println("Do you want to sign out from your account? Y / N");
+                    finalTouch = scanner.next();
+                    if (finalTouch.equals("Y")) {
+                        operations(users);
+                    }
+                    else break;
+                case EXIT:
+                    System.err.println("Do you want to exit? Y/N");
+                    finalTouch = scanner.next();
+                    if (finalTouch.equals("Y")) {
+                        System.exit(0);
+                        System.out.println("Thank you for using our services! Goodbye :D ");
+                    }
+                    else break;
+                default:
+                    System.out.println("Invalid option");
+                    break;
+            }
+        }
     }
 
-//    private static void updateBalance(Scanner value, String intention, User user) {
+    private static void updateBalance(Scanner value, String intention, User loggedInUser) {
+
+        double withdraw;
+        System.out.println("Set amount: ");
+        withdraw = value.nextDouble();
+
+        if (withdraw <= loggedInUser.getBalance()) {
+            if (intention.equals(WITHDRAW)) {
+
+                loggedInUser.setBalance(loggedInUser.getBalance() - withdraw);
+            } else {
+                loggedInUser.setBalance(loggedInUser.getBalance() + withdraw);
+            }
+        } else System.err.println("Impossible to Withdraw due to account state");
+
+        System.out.println("Current balance: " + loggedInUser.getBalance() + "\n");
+    }
+
+    private static void transfers (User loggedInUser, Scanner scanner, List<User> users){
+
+        double amount, userNumber;
+
+        System.out.println("How much to transfer?: ");
+        amount = scanner.nextDouble();
+        if (amount < 0) {
+            System.err.println("You can not send negative values of transfer");
+        }
+        System.out.println("To which account does the transfer is implementing? (User1-5)");
+        userNumber = scanner.nextDouble();
+//        users.forEach(user -> {
 //
-//
-//        System.out.println("Set amount: ");
-//        if (intention.equals(WITHDRAW)) {
-//            user.setBalance(user.getBalance() - value.nextDouble());
-//        } else {
-//            user.setBalance(user.getBalance() + value.nextDouble());
-//        }
-//        System.out.println("Current balance: " + user.getBalance() + "\n");
-//    }
+//        });
+        for (User user : users) {
+            if (user.getLogin().equals(String.valueOf(userNumber).replace(".0",""))) {
+                loggedInUser.setBalance(loggedInUser.getBalance() - amount);
+                user.setBalance(user.getBalance() + amount);
+                System.err.println("user");
+                System.out.println("Transfered " + amount + " to another account\nCurrent balance: " + user.getBalance() + "\nTransfered successfuly\n");
+
+                break;
+            }
+        }
+    }
 }
 
-
-// Let the account work
-// with - balance
-// card number for each user .
+// Card number .
